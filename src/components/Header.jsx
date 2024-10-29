298b0cd0856f2e901272016e789a172fc98954b9
FatherofElias <supra_3_8@hotmail.com>
2024-10-29 17:10:43 -0500

    upsate

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,18 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
+import './NavLinks.css';
 
 const Header = () => {
     return (
         <header>
             <h1>Marvel Comics Explorer</h1>
+            <nav>
+                <ul>
+                    <li><NavLink to="/character" activeClassName="active">Browse Characters</NavLink></li>
+                    <li><NavLink to="/comics" activeClassName="active">Comics</NavLink></li>
+                    <li><NavLink to="/" activeClassName="active">Home</NavLink></li>
+                </ul>
+            </nav>
         </header>
     );
 };
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,19 +1,11 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom';
-import './NavLinks.css';
+
 
 const Home = () => {
     return (
         <div>
             <h2>Home</h2>
             <p>Welcome to the Marvel Comics Explorer!</p>
-            <nav>
-                <ul>
-                    <li><NavLink to="/character" activeClassName="active">Browse Characters</NavLink></li>
-                    <li><NavLink to="/comics" activeClassName="active">Comics</NavLink></li>
-                    <li><NavLink to="/" activeClassName="active">Home</NavLink></li>
-                </ul>
-            </nav>
         </div>
     );
 };
